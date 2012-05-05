c31f9edad4b7764ba9f7bdcdc90786a5e58b944c
AbdelHakim Deneche <adeneche@gmail.com>
2012-05-05 15:03:13 +0100

    faster fitness

--- Fitness2I.m
+++ Fitness2I.m
@@ -4,24 +4,18 @@ function [ fit ] = Fitness2I( ind )
 global prblm;
 
 colors = zeros(prblm.N, 1); % couleur de chaque node
+freecols = ones(prblm.N, prblm.N); % couleurs dispos pour chaque node
 
 for i = 1:prblm.N % pour chaque noeud
     
-    adjn = prblm.adj(ind(i), :); % nodes adjacents à ind(i)
-    cols = colors(adjn>0); % couleurs des nodes adjacents à ind(i)
-    cols = cols(cols>0); % garder uniquement les couleurs non 0
-    
-    if (isempty(cols))
-        c = 1;
-    else
-        freecols = 1:prblm.N; % couleurs disponibles
-        freecols(cols(:)) = 0; % couleurs occupées par les adjacents à ind(i)    
-        freecols = find(freecols); % n° des couleurs libres
-        c = freecols(1);
-    end
-    
-    % le node i a la couleur c
+    fcols = find(freecols(ind(i),:)); % couleurs disponibles pour le node ind(i)
+    c = fcols(1); % choisir la 1ère couleur disponible
     colors(ind(i)) = c;
+    
+    % mettre à jour les couleurs disponibles pour les nodes adjacents à
+    % ind(i)
+    adjn = prblm.adj(ind(i), :); % nodes adjacents à ind(i)
+    freecols(adjn>0, c) = 0; % la couleur c n'est plus dispo pour ces nodes
 end
 
 % tous les nodes ont été colorés
